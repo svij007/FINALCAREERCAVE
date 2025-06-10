d9da19ddb418952a980f26bdc1a83578c442d87a
druvan-14 <kingsdameme@gmail.com>
2025-06-10 17:51:30 -0500

    Update error.js

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -10,12 +10,12 @@ export const errorMiddleware = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
 
   if (err.name === "CastError") {
-    const message = `Resource not found. Invalid ${err.path}`;
-    err = new ErrorHandler(message, 400);
+    const message = `Resource not found. Invalid ${err.path}`,
+      err = new ErrorHandler(message, 400);
   }
   if (err.code === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
-    err = new ErrorHandler(message, 400);
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
+      err = new ErrorHandler(message, 400);
   }
   if (err.name === "JsonWebTokenError") {
     const message = `Json Web Token is invalid, Try again!`;
@@ -25,7 +25,6 @@ export const errorMiddleware = (err, req, res, next) => {
     const message = `Json Web Token is expired, Try again!`;
     err = new ErrorHandler(message, 400);
   }
-
   return res.status(err.statusCode).json({
     success: false,
     message: err.message,

1b0a783728bb3d5289d0fec0f711b783f99bc152
druvan-14 <kingsdameme@gmail.com>
2025-06-10 17:34:11 -0500

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,6 @@
 import { User } from "../models/userSchema.js";
 import { catchAsyncErrors } from "./catchAsyncError.js";
-import ErrorHandler from "./error.js";
+import { ErrorHandler } from "../middlewares/error.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {

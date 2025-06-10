77fb0471bc2c7dd129faf2078692139e020f0570
druvan-14 <kingsdameme@gmail.com>
2025-06-10 16:22:16 -0500

    Update applicationRoutes.js

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+\import express from "express";
 import {
   employerGetAllApplications,
   jobseekerGetAllApplications,
@@ -9,9 +9,16 @@ import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
+// Jobseeker posts an application to a job
 router.post("/post", isAuthenticated, postApplication);
+
+// Employer retrieves all applications for their posted jobs
 router.get("/employer/getall", isAuthenticated, employerGetAllApplications);
+
+// Jobseeker retrieves all applications they’ve submitted
 router.get("/jobseeker/getall", isAuthenticated, jobseekerGetAllApplications);
+
+// Jobseeker deletes an application by ID
 router.delete("/delete/:id", isAuthenticated, jobseekerDeleteApplication);
 
 export default router;

13d6566fe6a48c6a29cb42e1fa794e64d05850f6
druvan-14 <kingsdameme@gmail.com>
2025-06-11 18:03:23 -0500

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -22,7 +22,7 @@ const { data } = await axios.post(
   { email, password, role },
   {
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "multipart/form-data",
     },
     withCredentials: true,
   }

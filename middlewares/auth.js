b7efa7f910770e2635d4d518b411eb93de6a46b9
druvan-14 <kingsdameme@gmail.com>
2025-06-11 22:28:01 -0500

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,10 @@ import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  //console.log(JSON.stringify(req));
+  
+  console.log('in auth.js');
+  console.log('cookies = ' + JSON.stringify(req.cookies));
   const token = req.cookies?.token;
-  //token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NDk5MGJlZjgxYjBkYTFmOTJkMTRmMCIsImlhdCI6MTc0OTY3OTQ1MSwiZXhwIjoxNzc1NTk5NDUxfQ.SaGyVzM6vhHEFPwoLBqIILeeylD6IMU_NwvfqrPLOU8";
-
 
   if (!token) {
     return next(new ErrorHandler("User Not Authorized - No Token", 401));
@@ -21,24 +21,12 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   }
 
   const user = await User.findById(decoded.id).select("-password");
-
-  {/* user = {
-        "_id": "684990bef81b0da1f92d14f0",
-        "name": "Druvan",
-        "email": "hello@gmail.com",
-        "phone": 3465889108,
-        "password": "$2a$10$Kh.HKG1qoqp7P3CmnIn9ZuE56ecZLSk7XQCyXrL5ApMxe/oY9ddJG",
-        "role": "Employer",
-        "createdAt": "2025-06-11T14:20:46.195Z",
-        "__v": 0
-    }; */}
   
   if (!user) {
     return next(new ErrorHandler("User not found", 404));
   }
 
   req.user = user;
-  console.log('in auth');
-  console.log(user);
+  console.log('user => ' + user);
   next();
 });

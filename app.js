5bc3b044b33af87a40ea045501f0e00fef23c4a1
druvan-14 <kingsdameme@gmail.com>
2025-06-11 19:53:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ config({ path: "./config/config.env" });
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })*/}
+app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");

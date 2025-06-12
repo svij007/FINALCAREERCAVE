0e89685b35c69afc88e531ed2fe4b662c07e94cd
druvan-14 <kingsdameme@gmail.com>
2025-06-12 14:42:20 -0500

    Update jobRoutes.js

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -12,7 +12,7 @@ import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.get("/getall", isAuthenticated,getAllJobs);
-router.post("/post",  isAuthenticated,postJob);
+router.post("/post", postJob);
 router.get("/getmyjobs", isAuthenticated, getMyJobs);
 router.put("/update/:id", isAuthenticated, updateJob);
 router.delete("/delete/:id", isAuthenticated, deleteJob);

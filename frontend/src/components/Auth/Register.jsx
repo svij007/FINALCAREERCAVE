c5327a7d9deb774e48b2af1a7866297f9ef72013
druvan-14 <kingsdameme@gmail.com>
2025-06-13 11:12:52 -0500

    Update Register.jsx

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -21,6 +21,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
+      const storedJwt = localStorage.getItem('token');
 const { data } = await axios.post(
   `${import.meta.env.VITE_BACKEND_URL}/api/v1/user/register`,
   { name, phone, email, role, password },

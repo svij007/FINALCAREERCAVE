fb1bc6bf1b5521f16ffe723804c13f373b707eee
druvan-14 <kingsdameme@gmail.com>
2025-06-13 11:12:02 -0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,8 +5,8 @@ import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/register", isAuthenticated, register);
-router.post("/login", isAuthenticated, login);
-router.get("/logout", isAuthenticated, logout);
+router.post("/login", login);
+router.get("/logout", logout);
 router.get("/getuser", isAuthenticated, getUser);
 
 export default router;

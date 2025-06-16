20e01129dd3661361cab155da486557a7abba2b4
svij007 <shlokvij12345@gmail.com>
2025-06-16 15:19:22 -0500

    Update PopularCompanies.jsx

--- frontend/src/components/Home/PopularCompanies.jsx
+++ frontend/src/components/Home/PopularCompanies.jsx
@@ -8,7 +8,7 @@ const PopularCompanies = () => {
       title: "The Comic Valut",
       location: "Cypress, Texas",
       openPositions: 10,
-      icon: <FaUserSecret />,
+      icon: <FaOpenBook />,
     },
     {
       id: 2,
